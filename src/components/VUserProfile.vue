f18580884830cc0836215bbc032e7b8207c45de5
Jhonatan Morais <jhonatanvinicius@gmail.com>
2019-07-05 20:08:27 -0300

    aula 3

---
+++ src/components/VUserProfile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="user-profile">
+    <img class="user-profile__avatar" :src="user.avatar_url" />
+    <div class="user-profile__name">{{ user.name }}</div>
+    <div class="user-profile__bio">{{ user.bio }}</div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    user: Object,
+    required: true,
+    default: () => ({})
+  }
+};
+</script>
+
---
+++ src/views/UserView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <v-user-search-form @submitted="searchUser"></v-user-search-form>
+    <v-user-profile :user="user"></v-user-profile>
+  </div>
+</template>
+<script>
+import { mapState } from "vuex";
+import VUserSearchForm from "@/components/VUserSearchForm";
+import VUserProfile from "@/components/VUserProfile";
+export default {
+  components: { VUserSearchForm, VUserProfile },
+  methods: {
+    searchUser(username) {
+      this.$store.dispatch("SEARCH_USER", { username });
+    }
+  },
+  computed: {
+    ...mapState({
+      user: "user"
+    })
+  }
+};
+</script>
+
